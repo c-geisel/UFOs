6e17f3aeaceec36409168aba105cfcd8eec7de11
Chloe Ittel <chloeittel@Chloes-MacBook-Air.local>
2021-10-22 09:45:25 -0400

    updates

---
+++ static/js/app.js
@@ -0,0 +1,5 @@
+// import the data from data.js, make variable to hold the data
+const tableData = data;
+
+// Reference the HTML table using d3, d3 is visualization library, use d3 to look for <tbody> tags in HTML
+var tbody = d3.select("tbody");
