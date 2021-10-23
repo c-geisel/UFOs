7f61309d41a5c3e4619286b9858c3843ff6ad430
Chloe Ittel <chloeittel@chloes-air.lan>
2021-10-22 21:04:51 -0400

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -2,4 +2,5 @@
 const tableData = data;
 
 // Reference the HTML table using d3, d3 is visualization library, use d3 to look for <tbody> tags in HTML
+// we are telling javascript where the data will be displayed
 var tbody = d3.select("tbody");
